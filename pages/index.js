1d746ee68c9387e99bb7cef34db329e2746b3dc6
yoonhero <yoonhero06@naver.com>
2023-07-17 23:33:33 +0900

    feat: Hell of console

--- pages/index.js
+++ pages/index.js
@@ -506,7 +506,7 @@ export default function Home() {
             />
             <div className='navbar bg-base-100 border-b-2'>
                 <div className='navbar-start'>
-                    {!auth && (
+                    {auth && (
                         <label className='btn btn-ghost' onClick={() => toggleContextDrawer()}>
                             <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
                                 <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h7' />
