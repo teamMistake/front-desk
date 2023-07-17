d442b71896ee34ecf4bc99800566ee062e4aab9f
one <imone075@gmail.com>
2023-07-17 18:45:40 +0900

    add og prop

--- pages/rank.js
+++ pages/rank.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { GhostButton } from "../components/button";
 import { useRouter } from "next/router";
 import { useUser } from "../hook/useUser";
-import Opengraph from "../components/opengraph";
+import { Opengraph } from "../components/opengraph";
 
 export default function Home() {
     const [ranks, setRanks] = useState([]);
@@ -58,7 +58,7 @@ export default function Home() {
 
     return (
         <>
-            <Opengraph title="" ogfitle="" description={`MOTA(모자)는 언어모델 "자모"를 기반으로 한 인공지능 채팅 서비스입니다. 자모는 GPT-3 같은 대규모 언어모델과 비등한 성능을 가지면서도, 낮은 성능의 컴퓨터에서도 구동이 가능할 수 있도록 만들어진 인공지능 언어 모델입니다. ChatGPT와 비교하자면 낮은 성능을 보이기는 하지만.... OpenAI는 몇 천억을 들여서 모델을 만들고 저희는 무자본으로 만들었는걸요. 이런 "자모”와 한번 대화해 볼래요?`} isMainPage={false} />
+            <Opengraph title="" ogfitle="" description={`MOJA(모자)는 언어모델 "자모"를 기반으로 한 인공지능 채팅 서비스입니다. 자모는 GPT-3 같은 대규모 언어모델과 비등한 성능을 가지면서도, 낮은 성능의 컴퓨터에서도 구동이 가능할 수 있도록 만들어진 인공지능 언어 모델입니다. ChatGPT와 비교하자면 낮은 성능을 보이기는 하지만... OpenAI는 몇 천억을 들여서 모델을 만들고 저희는 무자본으로 만들었는걸요. 이런 "자모”와 한번 대화해 볼래요?`} isMainPage={false} />
             <div className='navbar bg-base-100 border-b-2'>
                 <div className='navbar-start'>
                     <GhostButton onClick={() => router.push("")}>MOJA</GhostButton>
