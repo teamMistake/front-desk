f4739ccd74451c9856eb43d6b441937f45b5197c
one <imone075@gmail.com>
2023-07-18 18:52:26 +0900

    fix: set max width in rank

--- pages/rank.js
+++ pages/rank.js
@@ -103,7 +103,7 @@ export default function Home() {
                 <div className="relative overflow-hidden h-full flex flex-col w-full">
                     <p className="font-bold text-3xl text-accent justify-center my-10 flex select-none">Rank</p>
                     <div className="flex-1 overflow-x-auto">
-                            <table className="table max-w-md">
+                            <table className="table flex justify-center max-w-md">
                                 <thead>
                                     <tr className="text-base text-content dark:bg-none select-none">
                                         <th>Top</th>
