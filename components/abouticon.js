d2e4140ffeb3ec2ce54555c2bdf33b98f9aa3336
one <imone075@gmail.com>
2023-07-19 21:15:02 +0900

    fix: about icon adjustment

--- components/abouticon.js
+++ components/abouticon.js
@@ -5,7 +5,7 @@ const AboutIcon = ({ width, height }) => {
             xmlns="http://www.w3.org/2000/svg"
             width={width + "px"}
             height={height + "px"}
-            viewBox="0 0 1053.000000 999.000000"
+            viewBox="0 50 1000.000000 1010.000000"
             preserveAspectRatio="xMidYMid meet"
         >
             <g transform="translate(0.000000,999.000000) scale(0.100000,-0.100000)" fill="currentColor" stroke="none">
