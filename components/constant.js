94e14ac5fb71d69fd4c3a1f36b214db9ef2cf210
yoonhero <yoonhero06@naver.com>
2023-07-20 19:16:48 +0900

    fix: None output error handling

--- components/constant.js
+++ components/constant.js
@@ -1,6 +1,7 @@
 const COMPUTING_LIMITATION_ERROR = "저희 서버는 현재 많은 사용자분들의 요청을 처리하고 있어 답변에 시간이 조금 걸릴 수 있습니다. 조금만 기다려 주시면 감사하겠습니다."
 const COMPUTING_FATAL_ERROR = "저희 서버에 문제가 발생한 것 같습니다. 오류를 yoonhero06@naver.com으로 제보해주세요..."
 const NON_INPUT_ERROR = "무언가를 입력해 주시겠어요?"
+const NON_OUTPUT_ERROR = "자모가 아무 말도 안했네요.."
 
 const LOGIN_TRIGGER_NUM = 5
 
@@ -19,4 +20,4 @@ const MULTIPLE_MESSAGES = "multiple"
 const MOJA_TITLE = "모자와 대화 해모자. Chat with MOJA."
 const MOJA_DESC = `MOJA(모자)는 언어모델 "자모"를 기반으로 한 인공지능 채팅 서비스입니다. 자모는 GPT-3 같은 대규모 언어모델과 비등한 성능을 가지면서도, 낮은 성능의 컴퓨터에서도 구동이 가능할 수 있도록 만들어진 인공지능 언어 모델입니다. ChatGPT와 비교하자면 낮은 성능을 보이기는 하지만… OpenAI는 몇천억을 들여서 모델을 만들고 저희는 무자본으로 만들었는걸요. 이런 “자모”와 한번 대화해 볼래요?`
 
-export {COMPUTING_LIMITATION_ERROR, COMPUTING_FATAL_ERROR, MSG_EVENT, LOGIN_EVENT, NON_INPUT_ERROR, USER, COMPUTER, LOGIN_TRIGGER_NUM, AB_MODEL_TEST_EVENT, RANK_RES_EVENT, SINGLE_MESSAGE, MULTIPLE_MESSAGES, MOJA_TITLE, MOJA_DESC, SHARED_CONTENT_EVENT}
+export {COMPUTING_LIMITATION_ERROR, COMPUTING_FATAL_ERROR, MSG_EVENT, LOGIN_EVENT, NON_INPUT_ERROR, USER, COMPUTER, LOGIN_TRIGGER_NUM, AB_MODEL_TEST_EVENT, RANK_RES_EVENT, SINGLE_MESSAGE, MULTIPLE_MESSAGES, MOJA_TITLE, MOJA_DESC, SHARED_CONTENT_EVENT, NON_OUTPUT_ERROR}
--- utils/parsing.js
+++ utils/parsing.js
@@ -1,22 +1,22 @@
-import { COMPUTER, MSG_EVENT, USER } from "../components/constant";
+import { COMPUTER, MSG_EVENT, NON_OUTPUT_ERROR, USER } from "../components/constant";
 
 const parsingChatItem = (chat) => {
     let chats = [];
     chat.map(({ messageId, req, resp, experiment }) => {
         const userChat = { talker: USER, prompt: [{ resp: req }], event: MSG_EVENT, onlive: false, messageId: messageId };
 
-        let targetResp = resp.filter((r) => r.text != "")
+        // let targetResp = resp.filter((r) => r.text != "" || !r.text)
         let parsedResp = targetResp.map(({ reqId, text, selected }) => {
-            return { resp: text, selected: selected, reqId: reqId };
+            return { resp: text != "" ? text : NON_OUTPUT_ERROR, selected: selected, reqId: reqId };
         });
     
         const comChat = { talker: COMPUTER, prompt: parsedResp, event: MSG_EVENT, onlive: false, messageId: messageId };
 
         chats.push(userChat);
+        chats.push(comChat);
 
-        if (targetResp.length != 0){
-            chats.push(comChat);
-        }
+        // if (targetResp.length != 0){
+        // }
     });
 
     return chats;
