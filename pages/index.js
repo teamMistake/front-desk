051f958b8c356e7319c83ea05dc3a9ad49fafa7c
yoonhero <yoonhero06@naver.com>
2023-07-21 20:40:27 +0900

    feat: About Page and Loading Error

--- pages/index.js
+++ pages/index.js
@@ -192,6 +192,7 @@ export default function Home() {
     };
 
     const startNewChat = () => {
+        setLoading(false)
         clearAll();
         toggleContextDrawer();
     };
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = {
   },
   plugins: [
     require('daisyui'),
+      require('tailwindcss-bg-patterns'),
   ],
   daisyui: {
     themes: false,
