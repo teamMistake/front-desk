9c1571e53b01ecdd1118de341c8daf1380a5c4f6
one <imone075@gmail.com>
2023-07-21 17:43:22 +0900

    fix: share query

--- pages/rank.js
+++ pages/rank.js
@@ -113,7 +113,7 @@ export default function Home() {
                             router.push({
                                 pathname: "/about",
                                 query: { share: contextId ? contextId : "" },
-                            }, `/about`)
+                            })
                         }
                     >
                         <AboutIcon width='30' height='30' />
@@ -124,7 +124,7 @@ export default function Home() {
                             router.push({
                                 pathname: "/",
                                 query: { share: contextId ? contextId : "" },
-                            }, `/`)
+                            })
                         }
                     >
                         <ContextIcon width='30' height='30' />
