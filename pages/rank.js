90f34bec417431d39581adfc1f93cd9e49bf6288
one <imone075@gmail.com>
2023-07-22 15:42:08 +0900

    fix: sticky at rank table

--- pages/rank.js
+++ pages/rank.js
@@ -135,10 +135,10 @@ export default function Home() {
             <main className='bg-base-100 flex flex-row h-full w-screen overflow-hidden'>
                 <div className='relative overflow-hidden h-full flex flex-col w-full drawer'>
                     <p className='font-bold text-3xl text-accent-content dark:text-white justify-center my-10 flex select-none'>Rank</p>
-                    <div className='table-container flex overflow-y-auto justify-center'>
+                    <div className='table-container flex justify-center'>
                         <table className='table max-w-md'>
                             <thead>
-                                <tr className='text-base text-content dark:bg-none select-none'>
+                                <tr className='sticky text-base text-content dark:bg-none select-none'>
                                     <th>Top</th>
                                     <th>Name</th>
                                     <th>Score</th>
